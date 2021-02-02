14044a21fda5da8f2c49dfaf2372036c16eef70a
SAHIL RAJAPKAR <70615373+THSSIR18@users.noreply.github.com>
2021-02-01 20:36:12 -0500

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,13 @@
+//Myles Lesser
+//String Demo
+//April 22,2015
+
+public class Greetings
+{
+	public static void main (String[] args)
+	{
+		String firstName = "Myles";
+		String lastName = "Lesser";
+		System.out.println("Hello, "+ firstName + " " + lastName + " !");
+	}
+}
